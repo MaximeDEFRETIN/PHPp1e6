f5b687bd06e8e3e19de7a62ed56198c2f72c7e96
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-13 08:49:41 +0100

    Correction

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
-$name = "Wawrzyniak";
-echo "Bonjour $name, comment vas-tu ?"
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Partiee 1 : exercice 6</title>
+  </head>
+  <body>
+    <?php
+    $name = "Wawrzyniak";
+    echo 'Bonjour ' . $name . ', comment vas-tu ?'
+    ?>
+  </body>
+</html>
